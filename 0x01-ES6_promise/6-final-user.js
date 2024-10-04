862a368a5f9bf7981240aa87962532d00bc0e04b
Katalyst99 <katleho333@gmail.com>
2024-10-04 14:48:58 +0000

    6. Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((resp) => {
+      resp.map((obj) => ({
+        status: obj.status,
+        value: obj.status === 'fulfilled' ? obj.value : obj.reason.toString(),
+      }));
+    });
+}
