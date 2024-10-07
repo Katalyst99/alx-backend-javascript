f40a746885788d6e59d18e925ea5259bbd34d15e
Katalyst99 <katleho333@gmail.com>
2024-10-07 16:36:13 +0000

    Fixing code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || typeof startString !== 'string' || startString === '') {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
-  return [...set].filter((val) => val && val.startsWith(startString))
+  return [...set].filter((val) => val.startsWith(startString))
     .map((val) => val.slice(startString.length)).join('-');
 }
