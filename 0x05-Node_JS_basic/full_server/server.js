2b7a329c21ff270c2e1374419780ab4084bf3e7f
Katalyst99 <katleho333@gmail.com>
2024-12-01 12:04:30 +0000

    8. Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,1 +1,13 @@
-import express from ''
+import express from 'express';
+import linkRoutes from './routes';
+
+const app = express();
+const port = 1245;
+
+linkRoutes(app);
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
+
+export default app;
+module.exports = app;
