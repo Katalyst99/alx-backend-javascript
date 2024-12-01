73bbe79f79987fddc03f3be324cd548bf8f5db66
Katalyst99 <katleho333@gmail.com>
2024-12-01 09:57:23 +0000

    Handler for database operations

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const readDatabase = fPath => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    const lines = data.trim().split('\n');
+    lines.shift();
+
+    const flds = {};
+    for (const line of lines) {
+      const student = line.split(',');
+      if (!flds[student[3]]) {
+        flds[student[3]] = [];
+      }
+      flds[student[3]].push(student[0]);
+    }
+    resolve(flds);
+  });
+});
+
+export default readDatabase;
+module.exports = readDatabase;
